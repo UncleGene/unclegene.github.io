1c9ff198f5085db3caf821e3b2a3c219578fd86a
Eugene Kalenkovich <rubify@softover.com>
2013-07-30 15:20:38 -0700

    Split \s+ example

--- source/downloads/code/split.rb
+++ source/downloads/code/split.rb
@@ -3,5 +3,7 @@ s.split(/ /)
 #=> ["", "To\tbe", "", "or\nnot\tto", "", "be"]
 s.split(/( )/)
 #=> ["", " ", "To\tbe", " ", "", " ", "or\nnot\tto", " ", "", " ", "be", " ", "", " "]
+> s.split /\s+/
+#=> ["", "To", "be", "or", "not", "to", "be"]
 s.split(' ')
 #=> ["To", "be", "or", "not", "to", "be"]
